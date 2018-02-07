3788cf3e3db2a958c2c901950823262e76c183ae
Juan Sánchez Almendro <juansanalme@gmail.com>
2018-02-07 13:03:38 +0100

    FPS camera

---
+++ Prototipo/Assets/New Folder/Scripts/MouseLook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+ 
+public class MouseLook : MonoBehaviour
+{
+    //public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    //private float yaw = 0.0f;
+    private float pitch = 0.0f;
+
+    void Update () {
+        //yaw += speedH * Input.GetAxis("Mouse X");
+        //pitch -= speedV * Input.GetAxis("Mouse Y");
+
+        //transform.Rotate(pitch, yaw, 0.0f);
+
+        pitch = -speedV * Input.GetAxis("Mouse Y");
+        transform.Rotate(pitch, 0.0f, 0.0f);
+    }
+}
--- Prototipo/Assets/New Folder/Scripts/PlayerController.cs
+++ Prototipo/Assets/New Folder/Scripts/PlayerController.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	void Update()
+	private Rigidbody rb;
+
+	public float speedH = 2.0f;
+
+    private float yaw = 0.0f;
+
+	void Start ()
 	{
-		var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+		rb = GetComponent<Rigidbody>();
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void FixedUpdate()
+	{
+		//var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
 		var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
-		transform.Rotate(0, x, 0);
+		//transform.Rotate(0, x, 0);
 		transform.Translate(0, 0, z);
+
+		yaw = speedH * Input.GetAxis("Mouse X");
+        transform.Rotate(0.0f, yaw, 0.0f);
 	}
-	
 }
